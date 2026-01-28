f3130935e3d81fc1b74ddd64348c262762a0e467
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-01-28 10:28:08 -0600

    add initial files

---
+++ lib/Suricata/Ping.pm
@@ -0,0 +1,105 @@
+package Suricata::Ping;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Suricata::Ping - The great new Suricata::Ping!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Suricata::Ping;
+
+    my $foo = Suricata::Ping->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-suricata-ping at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Suricata-Ping>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Suricata::Ping
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Suricata-Ping>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Suricata-Ping>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Suricata-Ping>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2026 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+
+=cut
+
+1; # End of Suricata::Ping
